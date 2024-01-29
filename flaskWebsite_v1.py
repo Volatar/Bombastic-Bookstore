546ec6fbd6dcb802925473111bfafb1fb836fd39
RagingPhoenix478 <98458480+RagingPhoenix478@users.noreply.github.com>
2024-01-29 04:56:10 -0500

    Add files via upload
    First iteration of our website in Flask

---
+++ flaskWebsite_v1.py
@@ -0,0 +1,17 @@
+# Bombastic Bookstore
+# Flask Website v1
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# This function gives us the current "home" page
+# This starter version runs locally, and the web browser URL is "http://127.0.0.1:5000/home"
+@app.route("/home")
+def home():
+    return "Hello World! This is the starter version of our website <h1>HELLO<h1>"
+
+
+if __name__ == "__main__":
+    app.run()
