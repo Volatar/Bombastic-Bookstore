706a4a30852ffe034213f51dea337be66f13bb01
Taylor Sanderson <sanderson.taylor@gmail.com>
2024-01-30 16:24:30 -0500

    pre meeting documents update

--- book_parser.py
+++ book_parser.py
@@ -14,7 +14,8 @@ if response.status_code == 200:
     # Locate the element(s) containing the list of books
     book_list_elements = soup.find_all('li')
 
-    # Scrape the text of book list. Starting at the 4th list value and Stopping after 1000 list values due to unneeded text before and after.
+    # Scrape the text of book list.
+    # Starting at the 4th list value and Stopping after 1000 list values due to unneeded text before and after.
     book_list = [element.get_text(strip=True) for element in book_list_elements[4:1000]]
 
     # For loop to print all books in list
