0ea5d60fabda5077f20f038ceb283e0a5c89399a
RagingPhoenix478 <98458480+RagingPhoenix478@users.noreply.github.com>
2024-02-02 03:48:39 -0500

    Adds login page function to main (initial version), adds login.html and login.css (the later being blank for now)

--- web_app_main.py
+++ web_app_main.py
@@ -1,7 +1,7 @@
 # Bombastic Bookstore
 # Flask Website v1
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -16,6 +16,18 @@ def home():
     # Render the home page content using the separate function
     content = get_home_content()
     return render_template("Home.html", content=content)
+
+# This function adds a preliminary login ("http://127.0.0.1:5000/login")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
