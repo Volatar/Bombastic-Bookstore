765b64c354613c33657d59084a203a6204c55179
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-02-18 14:17:25 -0500

    styling

--- web_app_main.py
+++ web_app_main.py
@@ -44,18 +44,18 @@ def display():
     return render_template('display.html')
 
 # This functions adds a placeholder display page, accessed by logging in
-@app.route("/inventory")
+@app.route("/Catalog")
 def inventory():
-    return render_template('inventory.html')
+    return render_template('Catalog.html', data_type='inventory')
 
-@app.route('/inventory/<data_type>')
+@app.route('/Catalog/<data_type>')
 def show_inventory(data_type):
     data = {
         'sells': 'This is the sells data.',
         'inventory': 'This is the inventory data.',
         'other': 'This is some other data.'
     }
-    return render_template('inventory.html', data_type=data_type, data=data.get(data_type, ''))
+    return render_template('Catalog.html', data_type=data_type, data=data.get(data_type, ''))
 
 
 if __name__ == "__main__":
