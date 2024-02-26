17f6d6b1e970fbb46c0d71ccf6fe58585591a65d
Taylor Sanderson <sanderson.taylor@gmail.com>
2024-02-26 15:54:03 -0500

    minor changes

--- web_app_main.py
+++ web_app_main.py
@@ -28,11 +28,6 @@ FLASK_APP = 'web_app_main.py'
 chart_cache = {}
 
 
-# Separate function for home page content
-def get_home_content():
-    return "Hello World! This is the starter version of our website <h1>HELLO<h1>"
-
-
 # This function gives us the current "home" page
 # This starter version runs locally, and the web browser URL is "http://127.0.0.1:5000/"
 @app.route("/")
@@ -112,6 +107,7 @@ def show_inventory(data_type):
 
     return render_template('catalog.html', data_type=data_type, data=data.get(data_type, ''))
 
+
 @app.route("/book_details/<title>")
 def book_details(title):
     # Fetch book details from your database
@@ -121,18 +117,19 @@ def book_details(title):
     book_data = cursor.fetchone()
     conn.close()
 
-    # Using google API due to no descritpion from open lib
+    # Using google API due to no description from open lib
     description = ""
-    google_books_api_url = f"https://www.googleapis.com/books/v1/volumes?q={title}" # items contains volumn info
+    google_books_api_url = f"https://www.googleapis.com/books/v1/volumes?q={title}"  # item contains volume info
     response = requests.get(google_books_api_url)
-    if response.status_code == 200:
+    if response.status_code == 200:  # only proceed if call is successful
         data = response.json()
         if 'items' in data and len(data['items']) > 0:
-            book_info = data['items'][0]['volumeInfo']  # Getting first results only. descrition is in VolumeInfo.
+            book_info = data['items'][0]['volumeInfo']  # Getting first results only. description is in VolumeInfo.
             if 'description' in book_info:
                 description = book_info['description']
 
     return render_template("book_details.html", book_data=book_data, description=description)
+
 
 # Checkout
 @app.route('/add_to_cart', methods=['POST'])
